59a3321403a0e70cce4917e7fcbcf5756fe30d0d
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2023-12-05 00:48:01 +0330

    [Panel] Add two dnd lists to pipelie creation page

--- panel/ui/src/app/pipelines/create/page.js
+++ panel/ui/src/app/pipelines/create/page.js
@@ -1,105 +1,223 @@
 'use client';
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Grid,
-  Column,
-  Search,
-  ContainedList,
-  ContainedListItem,
-  Layer,
-} from '@carbon/react';
-import { useEffect, useState } from 'react';
+import React, { Component } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { Breadcrumb, BreadcrumbItem, Column, Grid } from '@carbon/react';
 
-export default function PipelineCreatorPage() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
+// fake data generator
+const getItems = (count, offset = 0) =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k + offset}`,
+    content: `item ${k + offset}`,
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250,
+});
+
+class CreatePipelinePage extends Component {
+  state = {
+    items: getItems(10),
+    selected: getItems(5, 10),
   };
 
-  const listItems = [
-    'List item 1',
-    'List item 2',
-    'List item 3',
-    'List item 4',
-  ];
+  /**
+   * A semi-generic way to handle multiple lists. Matches
+   * the IDs of the droppable container to the names of the
+   * source arrays stored in the state.
+   */
+  id2List = {
+    droppable: 'items',
+    droppable2: 'selected',
+  };
 
-  useEffect(() => {
-    const results = listItems.filter((listItem) =>
-      listItem.toLowerCase().includes(searchTerm.toLowerCase()),
-    );
-    setSearchResults(results);
-  }, [searchTerm]);
+  getList = id => this.state[this.id2List[id]];
 
-  return (
-    <Grid className='create-page' fullWidth>
+  onDragEnd = result => {
+    const { source, destination } = result;
 
-      <Column lg={16} md={8} sm={4} className='create-page_header'>
-        <Breadcrumb noTrailingSlash>
-          <BreadcrumbItem>
-            <a href='/'>Home</a>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <a href='/pipelines'>Pipelines</a>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            Create a new pipeline
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <h1 className='create-page__heading'>Create a new pipeline</h1>
-      </Column>
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
 
-      <Column lg={16} md={8} sm={4} className='create-page_main'>
-        <Grid>
+    if (source.droppableId === destination.droppableId) {
+      const items = reorder(
+        this.getList(source.droppableId),
+        source.index,
+        destination.index,
+      );
+
+      let state = { items };
+
+      if (source.droppableId === 'droppable2') {
+        state = { selected: items };
+      }
+
+      this.setState(state);
+    } else {
+      const result = move(
+        this.getList(source.droppableId),
+        this.getList(destination.droppableId),
+        source,
+        destination,
+      );
+
+      this.setState({
+        items: result.droppable,
+        selected: result.droppable2,
+      });
+    }
+  };
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
+  render() {
+    return (
+      <Grid className='create-page' fullWidth>
+        <Column lg={16} md={8} sm={4} className='create-page_header' style={{ marginBottom: '48px' }}>
+          <Breadcrumb noTrailingSlash>
+            <BreadcrumbItem>
+              <a href='/'>Home</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <a href='/pipelines'>Pipelines</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              Create a new pipeline
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <h1 className='create-page__heading'>Create a new pipeline</h1>
+        </Column>
+
+        <DragDropContext onDragEnd={this.onDragEnd}>
 
           <Column md={4} lg={7} sm={4}>
-            {/*<ContainedList kind='on-page' action={''}>*/}
-            {/*  {searchResults.map((listItem, key) => (*/}
-            {/*    <ContainedListItem key={key}>{listItem}</ContainedListItem>*/}
-            {/*  ))}*/}
-            {/*</ContainedList>*/}
-            <div
-              style={
-                {
-                  marginTop: '48px',
-                  height: '100px',
-                  border: '2px dashed gray',
-                  borderColor: '#d2d2d2',
-                  borderRadius: '5px',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }
-              }
-            >
-              <p>Drop a processor here</p>
-            </div>
+            <Droppable droppableId='droppable'>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}>
+                  {this.state.items.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style,
+                          )}>
+                          {item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+
+            {/* TODO: It must be visible if the pipeline is empty. */}
+            {/*<div*/}
+            {/*  style={*/}
+            {/*    {*/}
+            {/*      height: '100px',*/}
+            {/*      border: '2px dashed gray',*/}
+            {/*      borderColor: '#d2d2d2',*/}
+            {/*      borderRadius: '5px',*/}
+            {/*      display: 'flex',*/}
+            {/*      alignItems: 'center',*/}
+            {/*      justifyContent: 'center',*/}
+            {/*    }*/}
+            {/*  }*/}
+            {/*>*/}
+            {/*  <p>Drop a processor here</p>*/}
+            {/*</div>*/}
           </Column>
 
           <Column md={4} lg={{ span: 6, offset: 8 }} sm={4}>
-            <Layer as='section'>
-              <ContainedList label='Processors' kind='on-page' action={''}>
-                <Search
-                  placeholder={`Search between ${listItems.length} processors`}
-                  value={searchTerm}
-                  onChange={handleChange}
-                  closeButtonLabelText='Clear search input'
-                  size='lg'
-                  labelText='Processors'
-                />
-                {searchResults.map((listItem, key) => (
-                  <ContainedListItem key={key}>{listItem}</ContainedListItem>
-                ))}
-              </ContainedList>
-            </Layer>
+            <Droppable droppableId='droppable2'>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}>
+                  {this.state.selected.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style,
+                          )}>
+                          {item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
           </Column>
 
-        </Grid>
-      </Column>
+        </DragDropContext>
+      </Grid>
+    );
+  }
+}
 
-    </Grid>
-  );
-};
-
+export default CreatePipelinePage;
