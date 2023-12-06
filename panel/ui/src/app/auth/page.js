876df650bcecc8811d956380d149ecb26ce58b53
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2023-12-07 01:25:47 +0330

    [Panel] Send email and password to server by Axios

--- panel/ui/src/app/auth/page.js
+++ panel/ui/src/app/auth/page.js
@@ -1,12 +1,21 @@
 'use client';
 
-import { Button, Column, Grid, TextInput } from '@carbon/react';
+import {
+  Button,
+  Column,
+  Grid,
+  InlineNotification,
+  TextInput,
+} from '@carbon/react';
 import axios from 'axios';
 import { useState } from 'react';
+import { Address } from 'src/settings';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
 
   return (
     <Grid className="login-page" fullWidth>
@@ -36,10 +45,52 @@ export default function LoginPage() {
           }}
         />
 
+        {errorMessage !== '' && (
+          <InlineNotification
+            aria-label="closes notification"
+            kind="error"
+            statusIconDescription="notification"
+            subtitle={errorMessage}
+            onClose={() => {
+              setErrorMessage('');
+            }}
+            style={{ marginBottom: '16px' }}
+          />
+        )}
+
         <Button
           style={{ marginBottom: '24px' }}
           onClick={async (event) => {
-            // TODO
+            axios
+              .post(
+                Address + '/api/v1/auth',
+                {
+                  email: email,
+                  password: password,
+                },
+                {
+                  // TODO: CORS error happens
+                  // withCredentials: true,
+                  rejectUnauthorized: false,
+                }
+              )
+              .then((response) => {
+                if (response.status === 200) {
+                  // TODO: Fix security issues
+                  localStorage.setItem('email', response.data.email);
+                  localStorage.setItem('token', response.data.token);
+                } else {
+                  setErrorMessage('Unexpected response from server');
+                }
+              })
+              .catch((err) => {
+                if (err.response) {
+                  setErrorMessage(err.response.data.message);
+                } else {
+                  setErrorMessage('Unknown Error');
+                  console.error(err);
+                }
+              });
           }}
         >
           Login
---
+++ panel/ui/src/settings.js
@@ -0,0 +1,1 @@
+export const Address = 'https://panel.localhost';
