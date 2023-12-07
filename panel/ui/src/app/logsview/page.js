a20d9385afe6077b0219bc359e3d609a5d6261db
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2023-12-08 00:29:50 +0330

    [Panel] Implement logview page

--- panel/ui/src/app/logsview/page.js
+++ panel/ui/src/app/logsview/page.js
@@ -4,38 +4,49 @@ import {
   Column,
   Content,
   Grid,
-  ListItem,
-  OrderedList,
+  InlineNotification,
   Theme,
 } from '@carbon/react';
 import MainHeader from '@/components/MainHeader/MainHeader';
 import { useEffect, useRef, useState } from 'react';
-import useWebSocket from 'react-use-websocket';
 import 'ws';
 
 export default function LogviewPage() {
+  // TODO: Move it to the settings.
+  const MAX_LOGS = 20;
+
+  const [logs, setLogs] = useState([]);
+  const [showErrorNotification, setShowErrorNotification] = useState(false);
+
   const connection = useRef(null);
-
   useEffect(() => {
-    const socket = new WebSocket('wss://panel.localhost/api/v1/logs/bind');
+    const socket = new WebSocket(
+      'wss://panel.localhost/api/v1/logs/bind/' + localStorage.getItem('token')
+    );
 
-    // Connection opened
     socket.addEventListener('open', (event) => {
-      console.log('connected');
-      // socket.send("Connection established")
+      setShowErrorNotification(false);
     });
 
-    // Listen for messages
+    socket.addEventListener('error', (event) => {
+      setShowErrorNotification(true);
+      console.log(event);
+    });
+
     socket.addEventListener('message', (event) => {
-      console.log('Message from server ', event.data);
+      setLogs((prevLogs) => {
+        if (prevLogs.length > MAX_LOGS) {
+          prevLogs.shift();
+        }
+        return [...prevLogs, JSON.parse(event.data.toString())];
+      });
+      console.log(event.data);
     });
 
     connection.current = socket;
 
-    return () => connection.close();
+    return () => connection.current.close();
   }, []);
-
-  const [logs, setLogs] = useState([]);
 
   return (
     <div>
@@ -51,9 +62,69 @@ export default function LogviewPage() {
             className="create-page_header"
             style={{ marginBottom: '48px' }}
           >
-            {logs.map((value, index) => {
-              return <p key={index}>{value}</p>;
-            })}
+            {showErrorNotification ? (
+              <InlineNotification
+                title="An error occured"
+                subtitle="Please see console logs to debug or refresh this page to try again. Make sure your login token is not expired."
+              />
+            ) : null}
+
+            {logs.length ? (
+              logs.map((value, index) => {
+                const d = new Date(value.created_at);
+
+                let levelColor;
+                switch (value.level) {
+                  case 'Info':
+                    levelColor = '#0077b6';
+                    break;
+                  case 'Warning':
+                    levelColor = '#ee9b00';
+                    break;
+                  case 'Error':
+                    levelColor = '#ef233c';
+                    break;
+                  case 'Panic':
+                    levelColor = '#d90429';
+                    break;
+                  default:
+                    levelColor = 'black';
+                    break;
+                }
+
+                return (
+                  <div
+                    key={index}
+                    style={{
+                      marginBottom: '10px',
+                    }}
+                  >
+                    <Grid className="logs" fullWidth>
+                      <Column lg={1} md={8} sm={1}>
+                        <p>
+                          {d.getHours()}:{d.getMinutes()}:{d.getSeconds()}
+                        </p>
+                      </Column>
+                      <Column lg={1} md={8} sm={1}>
+                        <p style={{ fontWeight: 'bold', color: levelColor }}>
+                          {value.level}
+                        </p>
+                      </Column>
+                      <Column lg={8} md={8} sm={4}>
+                        <p>{value.message}</p>
+                      </Column>
+                      <Column lg={6} md={8} sm={4}>
+                        <p style={{ marginLeft: '10px' }}>
+                          {JSON.stringify(value.extra)}
+                        </p>
+                      </Column>
+                    </Grid>
+                  </div>
+                );
+              })
+            ) : !showErrorNotification ? (
+              <p>No log published yet.</p>
+            ) : null}
           </Column>
         </Grid>
       </Content>
