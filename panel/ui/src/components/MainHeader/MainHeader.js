12d0616f2d789d48e5be555f081bc31f602d495c
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2023-12-10 16:39:10 +0330

    [Panel] Add right panel to header

--- panel/ui/src/components/MainHeader/MainHeader.js
+++ panel/ui/src/components/MainHeader/MainHeader.js
@@ -11,8 +11,20 @@ import {
   SideNav,
   SideNavItems,
   HeaderSideNavItems,
+  SideNavMenu,
+  SideNavMenuItem,
+  SideNavLink,
+  StoryContent,
+  HeaderMenu,
+  SwitcherItem,
+  HeaderPanel,
+  SwitcherDivider,
+  Switcher,
 } from '@carbon/react';
-import { Switcher, Notification, UserAvatar } from '@carbon/icons-react';
+import {
+  Switcher as SwitcherIcon,
+  UserAvatar as UserAvatarIcon,
+} from '@carbon/icons-react';
 
 import Link from 'next/link';
 
@@ -32,49 +44,82 @@ const MainHeader = () => (
         </Link>
 
         <HeaderNavigation aria-label="PlutoEngine">
-          <Link href="/pipelines/create" passHref legacyBehavior>
-            <HeaderMenuItem>Create a new pipeline</HeaderMenuItem>
+          <Link href="/pipelines" passHref legacyBehavior>
+            <HeaderMenuItem>Pipelines</HeaderMenuItem>
           </Link>
           <Link href="/logsview" passHref legacyBehavior>
             <HeaderMenuItem>Watch logs in real-time</HeaderMenuItem>
           </Link>
         </HeaderNavigation>
 
-        <SideNav
-          aria-label="Side navigation"
-          expanded={isSideNavExpanded}
-          isPersistent={false}
-        >
-          <SideNavItems>
-            <HeaderSideNavItems>
-              <Link href="/pipelines/create" passHref legacyBehavior>
-                <HeaderMenuItem>Create a new pipeline</HeaderMenuItem>
-              </Link>
-
-              <Link href="/logsview" passHref legacyBehavior>
-                <HeaderMenuItem>Watch logs in real-time</HeaderMenuItem>
-              </Link>
-            </HeaderSideNavItems>
-          </SideNavItems>
-        </SideNav>
-
         <HeaderGlobalBar>
-          <HeaderGlobalAction
-            aria-label="Notifications"
-            tooltipAlignment="center"
-          >
-            <Notification size={20} />
+          {/*<HeaderGlobalAction*/}
+          {/*  aria-label="Notifications"*/}
+          {/*  tooltipAlignment="center"*/}
+          {/*>*/}
+          {/*  <Notification size={20} />*/}
+          {/*</HeaderGlobalAction>*/}
+          <HeaderGlobalAction aria-label="User" tooltipAlignment="center">
+            <UserAvatarIcon size={20} />
           </HeaderGlobalAction>
           <HeaderGlobalAction
-            aria-label="User Avatar"
-            tooltipAlignment="center"
+            aria-label={isSideNavExpanded ? 'Close switcher' : 'Open switcher'}
+            aria-expanded={isSideNavExpanded}
+            isActive={isSideNavExpanded}
+            onClick={onClickSideNavExpand}
+            tooltipAlignment="end"
+            id="switcher-button"
           >
-            <UserAvatar size={20} />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="App Switcher" tooltipAlignment="end">
-            <Switcher size={20} />
+            <SwitcherIcon size={20} />
           </HeaderGlobalAction>
         </HeaderGlobalBar>
+
+        <HeaderPanel
+          expanded={isSideNavExpanded}
+          onHeaderPanelFocus={onClickSideNavExpand}
+          href="#switcher-button"
+        >
+          <Switcher aria-label="Switcher" expanded={isSideNavExpanded}>
+            <SwitcherItem aria-label="Home" href="/">
+              Home
+            </SwitcherItem>
+            <SwitcherItem aria-label="Pipelines" href="/pipelines">
+              Pipelines
+            </SwitcherItem>
+            <SwitcherItem href="/logsview" aria-label="Watch logs in real-time">
+              Watch logs in real-time
+            </SwitcherItem>
+            <SwitcherDivider />
+
+            {/*TODO*/}
+            {/*<SwitcherItem href='#' aria-label='Link 3'>*/}
+            {/*  Settings*/}
+            {/*</SwitcherItem>*/}
+            {/*<SwitcherItem href='#' aria-label='Link 4'>*/}
+            {/*  Backup*/}
+            {/*</SwitcherItem>*/}
+            {/*<SwitcherDivider />*/}
+
+            <SwitcherItem
+              href="https://plutoengine.ir/docs/"
+              aria-label="Documentation"
+            >
+              Documentation
+            </SwitcherItem>
+            <SwitcherItem
+              href="https://t.me/PlutoEngineCommunity"
+              aria-label="Community"
+            >
+              Community
+            </SwitcherItem>
+            <SwitcherItem
+              href="https://github.com/0x9n0p/pluto"
+              aria-label="Source Code"
+            >
+              Source Code
+            </SwitcherItem>
+          </Switcher>
+        </HeaderPanel>
       </Header>
     )}
   />
@@ -107,7 +152,7 @@ export const UnAuthenticatedHeader = () => (
             <UserAvatar size={20} />
           </HeaderGlobalAction>
           <HeaderGlobalAction aria-label="App Switcher" tooltipAlignment="end">
-            <Switcher size={20} />
+            <SwitcherIcon size={20} />
           </HeaderGlobalAction>
         </HeaderGlobalBar>
       </Header>
